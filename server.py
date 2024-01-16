61cf8e32fc7d5fb0f29f5018858836f7b4da963a
electroblats <adam.laurell2@gmail.com>
2024-01-16 19:17:07 +0100

    1

--- server.py
+++ server.py
@@ -83,8 +83,9 @@ def main():
     # client_manager = fl.server.client_manager.SimpleClientManager.wait_for(num_clients=2, timeout=10)
     # client_manager = fl.server.SimpleClientManager.wait_for(num_clients=2, timeout=10)
     # ClientManager = fl.server.ClientManager.wait_for(num_clients=2, timeout=10)
-    ClientManager = fl.server.ClientManager()
-    client_manager = fl.server.client_manager.SimpleClientManager.wait_for(ClientManager,num_clients=3,timeout=10)
+    
+    ClientManager = fl.server.client_manager.SimpleClientManager()
+    client_manager = fl.server.client_manager.ClientManager.wait_for(ClientManager,num_clients=args.num_clients,timeout=10)
 
     # Start Flower server
     fl.server.start_server(
